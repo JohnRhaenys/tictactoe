f87cd2b5adc7cd0a3423b1caa27207086fb5c888
JohnRhaenys <contato.joaovitorandrade@gmail.com>
2022-11-19 20:09:00 -0300

    UI assets and events added

--- src/main/java/com/tictactoe/WindowController.java
+++ src/main/java/com/tictactoe/WindowController.java
@@ -1,14 +1,38 @@
 package com.tictactoe;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+
+import static com.tictactoe.utils.Constants.GO_SIGNAL;
+import static com.tictactoe.utils.Constants.WAIT_SIGNAL;
 
 public class WindowController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button startButton;
+
+    @FXML
+    private ImageView circleImage;
+
+    @FXML
+    private ImageView xImage;
+
+    @FXML
+    private Label xPlayerSignalLabel;
+
+    @FXML
+    private Label circlePlayerSignalLabel;
+
+    @FXML
+    private GridPane gameGrid;
+
+    @FXML
+    void startButtonPressed(ActionEvent event) {
+        circlePlayerSignalLabel.setText(GO_SIGNAL);
+        xPlayerSignalLabel.setText(WAIT_SIGNAL);
     }
 }
--- src/main/java/com/tictactoe/utils/Constants.java
+++ src/main/java/com/tictactoe/utils/Constants.java
@@ -2,4 +2,7 @@ package com.tictactoe.utils;
 
 public class Constants {
     public static final String GAME_NAME = "TIC TAC TOE";
+    public static final String GO_SIGNAL = "GO!";
+    public static final String WAIT_SIGNAL = "WAIT";
 }
+

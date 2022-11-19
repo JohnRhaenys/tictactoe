9ebd33c24c5377d5389055a22fbb5e1e5b79befc
JohnRhaenys <contato.joaovitorandrade@gmail.com>
2022-11-19 18:56:01 -0300

    rename resources and files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,6 @@
 module com.example.tictactoe {
     requires javafx.controls;
     requires javafx.fxml;
-
-
-    opens com.example.tictactoe to javafx.fxml;
-    exports com.example.tictactoe;
+    opens com.tictactoe to javafx.fxml;
+    exports com.tictactoe;
 }
